c72c7843f2886d7c814aebac79da1e4b23144c85
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-27 17:47:33 +0900

    Update app001.py

--- app001.py
+++ app001.py
@@ -1,9 +1,22 @@
-from flask import Flask
+# app.py
+
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask!'
+def index():
+    return render_template('index.html')
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    # フォームからのデータを取得
+    n_value = request.form.get('N')
+    a_value = request.form.get('A')
+    b_value = request.form.get('B')
+
+    # データを表示
+    return f'N: {n_value}, A: {a_value}, B: {b_value}'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True)
